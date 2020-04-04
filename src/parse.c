307d0aae098739baca339659ad74305202f10d96
Yunfei Jing <jinyj@student.unimelb.edu.au>
2020-04-04 16:20:17 +1100

    test

--- src/parse.c
+++ src/parse.c
@@ -32,34 +32,34 @@ void recv_timeout(int s , int timeout, char* response)
     //beginning time
     gettimeofday(&begin , NULL);
 
-//    while(1)
-//    {
-////        gettimeofday(&now , NULL);
-////
-////        //time elapsed in seconds
-////        timediff = (double)(now.tv_sec - begin.tv_sec) + 1e-6 * (double)(now.tv_usec - begin.tv_usec);
-////
-////        //if you got some data, then break after timeout
-////        if( total_size > 0 && timediff > timeout )
-////        {
-////            break;
-////        }
-//
-////        memset(chunk , 0 , MAX_BUFFER);	//clear the variable
-//        if((size_recv =  recv(s , &chunk[total_size] , sizeof chunk - size_recv , 0) ) < 0)
-//        {
-//            //if nothing was received then we want to wait a little before trying again, 0.1 seconds
-//            break;
-//        }
-//        else
-//        {
-//            total_size += size_recv;
-////            printf("%s" , chunk);
-//            //reset beginning time
-////            gettimeofday(&begin , NULL);
-//        }
-//
-//    }
+    while(1)
+    {
+        gettimeofday(&now , NULL);
+
+        //time elapsed in seconds
+        timediff = (double)(now.tv_sec - begin.tv_sec) + 1e-6 * (double)(now.tv_usec - begin.tv_usec);
+
+        //if you got some data, then break after timeout
+        if( total_size > 0 && timediff > timeout )
+        {
+            break;
+        }
+
+        memset(chunk , 0 , MAX_BUFFER);	//clear the variable
+        if((size_recv =  recv(s , &chunk[total_size] , sizeof chunk - size_recv , 0) ) < 0)
+        {
+            //if nothing was received then we want to wait a little before trying again, 0.1 seconds
+            break;
+        }
+        else
+        {
+            total_size += size_recv;
+//            printf("%s" , chunk);
+            //reset beginning time
+            gettimeofday(&begin , NULL);
+        }
+
+    }
 
     int read = 0;
     int ptr = 0;
