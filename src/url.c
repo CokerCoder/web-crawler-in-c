1f84fc874243a38229c05848700bb99b51b1d46d
Yunfei Jing <jinyj@student.unimelb.edu.au>
2020-04-05 17:09:06 +1000

    fix relative paths

--- src/url.c
+++ src/url.c
@@ -105,7 +105,7 @@ void to_abs(char* relative, char* host, char* path) {
         else {
             // Retrieve the pointer to the last occurrence of the '/'
             char* last_dir = strrchr(path, '/');
-            memcpy(last_dir+1, relative, strlen(relative)-1);
+            memcpy(last_dir+1, relative, strlen(relative));
 
             sprintf(abs, "http://%s%s\n", host, path);
         }
